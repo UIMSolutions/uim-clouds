83ac7026ec42fb3b4cc1e0a9a1178ca63716f1b1
ONS <ui.manufaktur@gmail.com>
2026-02-05 16:39:38 +0100

    Aktualisieren von types.d

--- namespaces/source/uim/namespaces/types.d
+++ namespaces/source/uim/namespaces/types.d
@@ -9,16 +9,6 @@ import std.conv : to;
 
 @safe:
 
-/// Enumeration of Linux namespace types
-enum NamespaceType {
-  PID = 0x20000000,      /// Process ID namespace
-  Network = 0x40000000,  /// Network namespace
-  IPC = 0x08000000,      /// Inter-process communication namespace
-  Mount = 0x00020000,    /// Mount filesystem namespace
-  UTS = 0x04000000,      /// UTS (hostname/domain) namespace
-  User = 0x10000000,     /// User and group ID namespace
-  Cgroup = 0x02000000    /// Cgroup namespace
-}
 
 /// String representation of namespace type
 string namespaceTypeToString(NamespaceType type) @safe {
