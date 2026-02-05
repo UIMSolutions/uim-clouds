1eed103c327b2547105ea023c69d693588f56924
ONS <ui.manufaktur@gmail.com>
2026-02-05 16:58:58 +0100

    Aktualisieren von types.d

--- namespaces/source/uim/namespaces/types.d
+++ namespaces/source/uim/namespaces/types.d
@@ -127,12 +127,3 @@ struct MountInfo {
   }
 }
 
-/// Network namespace information
-struct NetworkNamespaceInfo {
-  int pid;               /// PID owning the namespace
-  string[] interfaces;   /// Network interfaces in this namespace
-  string[] routes;       /// Routes in this namespace
-  bool hasIpv4;          /// Has IPv4 configured
-  bool hasIpv6;          /// Has IPv6 configured
-}
-
